2d546508293beae920b968e86395babd0b795362
Ирина Проскурякова <pussuphomies@gmail.com>
2024-10-08 16:35:19 +0700

    Имя студента

--- app.py
+++ app.py
@@ -180,4 +180,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-     return render_template('example.html')
+     name = 'Ирина Проскурякова'
+     return render_template('example.html', name=name)
