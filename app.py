8fcb44ba5acac3519aa2d4448e5e0d5d26e3c71c
Ирина Проскурякова <pussuphomies@gmail.com>
2024-10-18 20:33:26 +0700

    Улучшения по выводу цветов

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, abort
 app = Flask(__name__)
 
 @app.route("/")
@@ -162,12 +162,12 @@ def personal():
 #      return 'со слэшем'
 
 flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
-     
+
 @app.route('/lab2/add_flower/<name>')
 def add_flower(name):
-     flower_list.append(name)
-     return f'''
-<!docktype html>
+    flower_list.append(name)
+    return f'''
+<!DOCTYPE html>
 <html>
     <body>
     <h1>Добавлен новый цветок</h1>
@@ -178,15 +178,68 @@ def add_flower(name):
 </html>
 '''
 
+@app.route('/lab2/flowers')
+def list_flowers():
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Список всех цветов</h1>
+    <p>Всего цветов: {len(flower_list)} </p>
+    <p>Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def show_flower(flower_id):
+    if flower_id < 0 or flower_id >= len(flower_list):
+        abort(404, 'цветок не найден')
+    
+    flower = flower_list[flower_id]
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Цветок</h1>
+    <p>Название цветка: {flower} </p>
+    <a href="/lab2/flowers">Вернуться к списку всех цветов</a>
+    </body>
+</html>
+'''
+@app.route('/lab2/clear_flowers')
+def clear_flowers():
+    global flower_list
+    flower_list = []
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Список цветов очищен</h1>
+    <a href="/lab2/flowers">Перейти к списку всех цветов</a>
+    </body>
+</html>
+'''
+
 @app.route('/lab2/example')
 def example():
-     name = 'Ирина Проскурякова'
-     number = '2'
-     group = 'ФБИ-24'
-     course = '3'
-     result = 5 + 2
-     result1 = 11 * 28
-     result2 = 3258 - 759
-     result3 = 8452/793
-     result4 = 45 ** 8
-     return render_template('example.html', name=name, number=number, group=group, course=course, result=result, result1=result1, result2=result2, result3=result3, result4=result4)
+     name, lab_num, group, course = 'Ирина Проскурякова', 2, 'ФБИ-24', 3
+     fruits = [
+          {'name': 'яблоки', 'price': 100 }, 
+          {'name': 'груши', 'price': 120},  
+          {'name': 'апельсины', 'price': 80}, 
+          {'name': 'мандарины', 'price': 95}, 
+          {'name': 'манго', 'price': 321}, 
+    ]
+     return render_template('example.html',
+                            name=name, lab_num=lab_num, group=group,
+                            course=course, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+     return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+     phrase = "0 <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
+     return render_template('filter.html', phrase = phrase)
