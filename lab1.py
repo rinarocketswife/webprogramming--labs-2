c5b7e97f9c90418a243a6caf9087c11ab09d663a
Ирина Проскурякова <pussuphomies@gmail.com>
2024-10-25 12:53:03 +0700

    Рефакторинг: выделение первой лабораторной

---
+++ lab1.py
@@ -0,0 +1,137 @@
+from flask import Blueprint, redirect, url_for
+lab1 = Blueprint('lab1', __name__)
+
+
+@lab1.route("/lab1")
+def lab():
+     return """
+<!doctype html>
+<html>
+    <head>
+        <title>Проскурякова Ирина Дмитриевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+
+        <h1>web-сервер на flask</h1>
+
+        </header>
+
+        <p>
+        Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </p>
+
+        <p>
+            <ul>
+                <li><a href="/menu">Меню</a></li>
+            </ul>
+        </p>
+
+        <h2>Реализованные роуты</h2>
+
+        <p>
+            <ul>
+                <li><a href="/lab1/oak">Дуб</a></li>
+                <li><a href="/lab1/student">Студент</a></li>
+                <li><a href="/lab1/python">Python</a></li>
+                <li><a href="/lab1/personal">Personal</a></li>
+            </ul>
+        </p>
+
+        <footer>
+            &copy; Ирина Проскурякова, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+
+@lab1.route('/lab1/oak')
+def oak():
+     return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + url_for('static', filename='oak.jpg') +'''">
+    </body>
+</html>
+'''
+
+
+@lab1.route('/lab1/student')
+def student():
+     return '''
+<!doctype html>
+<html>
+    <head>
+        <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        <title>Студент</title>
+    </head>
+    <body>
+        <h1>Проскурякова Ирина Дмитриевна</h1>
+        <img src="''' + url_for('static', filename='logo.png') + '''" class="Student">
+    </body>
+</html>
+'''
+
+
+@lab1.route('/lab1/python')
+def python():
+     return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        <title>Python</title>
+    </head>
+    <body>
+        <h1>Что такое Python?</h1>
+        <p>
+            Python – это высокоуровневый язык программирования, который был разработан в конце 1980-х годов. Его разработчик, Гвидо ван Россум, вложил в основу языка простоту и читабельность кода, что позволяет 
+            использовать Python для быстрой и эффективной разработки. Много популярных веб-сайтов, компьютерных игр и программ, написанных на Python, вы используете ежедневно: Dropbox, Uber, Sims, Google, GIMP и другие.
+        </p>
+        <p>
+            Язык отличается понятным синтаксисом, поэтому Python подходит для начинающих программистов. Он широко используется 
+            во многих областях: веб-разработка, научные исследования, анализ данных, искусственный интеллект, машинное обучение, разработка игр. 
+        </p>
+        <img src="''' + url_for('static', filename='python.jpg') + '''"class="Python">
+    </body>
+</html>
+'''
+
+
+@lab1.route('/lab1/personal')
+def personal():
+     return '''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        <title>NIKE</title>
+    </head>
+    <body>
+                <h1>NIKE</h1>
+        <p>
+            Nike — американская транснациональная компания, специализирующаяся на спортивной одежде и обуви. 
+            Штаб-квартира — в городе Бивертон (штат Орегон).
+        </p>
+        <p>
+           Почти вся продукция Nike производится сторонними компаниями-подрядчиками вне территории США (в основном в Азии), сама компания является правообладателем торговых марок, разрабатывает дизайн продукции и владеет сетью магазинов 
+           (около 1150 по всему миру), а также торговых центров NikeTown. С 20 сентября 2013 года входит в Промышленный индекс Доу Джонса. Nike является самым дорогим спортивным брендом в мире.
+        </p>
+        <p>
+        Компания, основанная 25 января 1964 года под названием Blue Ribbon Sports, официально стала Nike, Inc. в 1978 году. Nike продаёт свою продукцию под собственным брендом, а также под марками Nike Golf, Nike Pro, Nike +, Air Jordan, Nike Blazers, Air Force 1, Nike Dunk, Air Max, Foamposite, Nike Skateboarding, Nike CR7, Hurley International, Converse. 
+        Nike является спонсором многих спортсменов и спортивных команд по всему миру. Начиная с 1990-х годов компания регулярно подвергается критике за то, что её продукция производится на фабриках, где нарушается трудовое законодательство.
+        </p>
+        <img src="''' + url_for('static', filename='nike.png') + '''"class="Car">
+    </body>
+</html>
+'''
