e0edf24d4d86b58b0ad3b165ab20ad4c92968096
Ирина Проскурякова <pussuphomies@gmail.com>
2024-11-08 22:27:31 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -127,6 +127,13 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'fred', 'password': '111'},
+    {'login': 'rick', 'password': '222'},
+]
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -135,8 +142,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('lab4/login.html', login=login, authorizes=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('lab4/login.html', login=login, authorizes=True)
     
     error = 'Неверный логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorizes=False)
